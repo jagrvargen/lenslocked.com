acc7348028a7cfbaf76c51c611444abf65ad984e
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-22 05:53:46 -0700

    finish chapter 3

--- main.go
+++ main.go
@@ -17,9 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">mailto:support@lenslocked.com</a>")
 }
 
+func custom404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Temp Custom 404 Page</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(custom404)
 	http.ListenAndServe(":3000", nil)
 }
