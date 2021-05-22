d5c24ed6481cefeb41c4dffbd7d64101a020ed39
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-22 05:37:50 -0700

    Revert "completed exercises 1 and 2"
    This reverts commit 4bf8e1087991293a05bd6df93d349153b47804fa.

--- main.go
+++ main.go
@@ -17,21 +17,9 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">mailto:support@lenslocked.com</a>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQs</h1><p>Nothing to see here yet</p>")
-}
-
-func custom404(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>My custom 404 not found page</h1>")
-}
-
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = http.HandlerFunc(custom404)
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", nil)
 }
