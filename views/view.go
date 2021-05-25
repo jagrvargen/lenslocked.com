839679a152c171b8d4a4265a0392317209e83f4b
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-25 02:49:47 -0700

    added render method to View type

--- views/view.go
+++ views/view.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
@@ -13,6 +14,11 @@ var (
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// Render renders a predefined layout.
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
 
 func NewView(layout string, files ...string) *View {
